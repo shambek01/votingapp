081389f278ad0d4f59018bce50f8365c12f81ee6
shambek01 <shambek01@gmail.com>
2021-03-11 07:12:53 +0600

    By Bekezhan 22000

--- src/main/java/com/thirdparty/votingapp/internal/controller/AdminController.java
+++ src/main/java/com/thirdparty/votingapp/internal/controller/AdminController.java
@@ -1,12 +1,11 @@
 package com.thirdparty.votingapp.internal.controller;
 
 import com.thirdparty.votingapp.internal.dto.PollDto;
+import com.thirdparty.votingapp.internal.repository.model.Group;
+import com.thirdparty.votingapp.internal.repository.model.Interest;
 import com.thirdparty.votingapp.internal.repository.model.Poll;
 import com.thirdparty.votingapp.internal.repository.model.Profile;
-import com.thirdparty.votingapp.internal.service.InterestService;
-import com.thirdparty.votingapp.internal.service.OptionService;
-import com.thirdparty.votingapp.internal.service.PollService;
-import com.thirdparty.votingapp.internal.service.ProfileDetailsService;
+import com.thirdparty.votingapp.internal.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,13 +21,15 @@ public class AdminController {
     private OptionService optionService;
     private ProfileDetailsService profileDetailsService;
     private InterestService interestService;
+    private GroupService groupService;
 
     @Autowired
-    public AdminController(PollService pollService, OptionService optionService, ProfileDetailsService profileDetailsService, InterestService interestService){
+    public AdminController(PollService pollService, OptionService optionService, ProfileDetailsService profileDetailsService, InterestService interestService, GroupService groupService){
         this.optionService = optionService;
         this.pollService = pollService;
         this.profileDetailsService = profileDetailsService;
         this.interestService = interestService;
+        this.groupService = groupService;
     }
 
 
@@ -41,6 +42,9 @@ public class AdminController {
         model.addAttribute("profileOptions", profile.getOptions());
         model.addAttribute("options", optionService.getAll());
         model.addAttribute("dtoPoll", new PollDto());
+        model.addAttribute("groups",groupService.getAll());
+        model.addAttribute("users", profileDetailsService.getAll());
+        model.addAttribute("currentUser", profile);
         return "admin";
     }
 
@@ -59,6 +63,31 @@ public class AdminController {
     @GetMapping("/option/delete/{id}")
     public String deleteOption(@PathVariable Long id){
         optionService.delete(id);
+        return "redirect:/admin";
+    }
+    @PostMapping("/interest/add")
+    public String addInterest(@ModelAttribute Interest interest){
+        interestService.add(interest);
+        return "redirect:/admin";
+    }
+    @GetMapping("/interest/delete/{id}")
+    public String deleteInterest(@PathVariable Long id){
+        interestService.delete(id);
+        return "redirect:/admin";
+    }
+    @PostMapping("/group/add")
+    public String addGroup(@ModelAttribute Group group){
+        groupService.add(group);
+        return "redirect:/admin";
+    }
+    @GetMapping("/group/delete/{id}")
+    public String deleteGroup(@PathVariable Long id){
+        groupService.delete(id);
+        return "redirect:/admin";
+    }
+    @GetMapping("/user/delete/{id}")
+    public String deleteProfile(@PathVariable Long id){
+        profileDetailsService.delete(id);
         return "redirect:/admin";
     }
 }
